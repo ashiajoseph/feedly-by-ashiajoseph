914e32a7c7e186c97213921f1be4618feaab7566
ashiajoseph <ashiajoseph@gmail.com>
2021-10-26 16:34:38 +0530

    Added left title and icons

---
+++ src/components/Navbar.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Typography, Button} from "@bigbinary/neetoui/v2";
+import { Search, Filter, Notification } from "@bigbinary/neeto-icons";
+
+
+function Navbar() {
+    return (
+        <div className="flex flex-row justify-between border-b-2 border-opacity-75">
+            <div className="p-3 m-2">
+            <Typography style="h4" weight="semibold" className="neeto-ui-text-gray-500">
+            Feed.ly
+            </Typography>
+            </div>
+            <div className="flex flex-row items-center ">
+                <Search className="mr-4"/>
+                <Notification className="mr-4"/>
+                <Button
+                onClick={()=> {}}
+                size="large"
+                label="Filter"
+                style="secondary"
+                icon={Filter}
+                className= "mr-5"
+              />
+
+            </div>
+        </div> 
+    )
+}
+
+export default Navbar
