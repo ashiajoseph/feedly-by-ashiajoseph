b164fe828cdcfccfe1b7a643bbe182e4ef2b7c1b
ashiajoseph <ashiajoseph@gmail.com>
2021-10-27 20:08:12 +0530

    Error page added

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { Typography, Button} from "@bigbinary/neetoui/v2";
+import { Home } from "@bigbinary/neeto-icons";
 
 function Error() {
-    return (
-        <div>
-         
+    const history = useHistory();
+    return ( 
+        <div className="flex flex-col justify-center items-center my-40">
+            <div className="mb-10">
+                <img src={process.env.PUBLIC_URL + '/error.jpg'} alt="Error diagram"/>
+            </div>
+            <Typography style="h3" weight="semibold" className="neeto-ui-text-gray-800  ">
+            You have landed somwhere
+            </Typography>
+            <Typography style="h3" weight="semibold" className="neeto-ui-text-gray-800 mb-7 ">
+            unknown.
+            </Typography>
+            
+            <Button
+                onClick={()=>{history.push('/')}}
+                size="large"
+                icon={Home}
+                label="Take me home"
+                style="secondary"
+                iconPosition="left"
+              />
         </div>
+
     )
 }
 
