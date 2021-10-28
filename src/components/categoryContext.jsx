d05157deb07835700361e0bbacd760a9d4c330ca
ashiajoseph <ashiajoseph@gmail.com>
2021-10-28 18:36:54 +0530

    Added more categories to pane

--- src/components/categoryContext.jsx
+++ src/components/categoryContext.jsx
@@ -4,7 +4,7 @@ import React, { createContext, useState, useRef }from "react"
 const categoryContext = createContext()
 
 const CategoryProvider = ({children}) => {
-    const categoryCheckbox = useRef({national: true,business: false, sports: true, world: false})
+    const categoryCheckbox = useRef({all: false, automobile: false, business: true, entertainment: false,national: true,politics: false, science: false, startup: false, sports: true, technology:false,  world: true   })
     const [save, toggleSave]= useState(true)
     return (
         <categoryContext.Provider value={[categoryCheckbox,toggleSave, save]} >
