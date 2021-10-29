3a07c5e904c12a561c01e6fbe6079e0721e53ae6
ashiajoseph <ashiajoseph@gmail.com>
2021-10-30 04:04:09 +0530

    Styling added

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,9 +1,10 @@
 import React, {setState, useEffect, useState, useContext} from 'react'
 import ReactDOM from "react-dom"
 import { Input } from "@bigbinary/neetoui/v2";
-import { Search,Close } from "@bigbinary/neeto-icons";
+import { Search,Close,Right } from "@bigbinary/neeto-icons";
 import { categoryContext } from './categoryContext';
 import { newsContext } from './Landing';
+import {Link} from 'react-router-dom'
 
 const SearchBar = ({openSearchBox,setOpenSearchBox}) => {
     const [keyword, setKeyword] = useState("")
@@ -17,7 +18,7 @@ const SearchBar = ({openSearchBox,setOpenSearchBox}) => {
     const category_array25= filteredCategoryList.map((category) => { return  [ category , newsDetails[category]]})
    
     category_array25.forEach(([category,array]) => {
-      let obj= array.map(({title,url})=> { return {title,url,category, details: array} })
+      let obj= array.map(({title,url,category, author, content,date, readMoreUrl,})=> { return {title,url,category, author, content,date, readMoreUrl, details: array} })
       searchList= searchList.concat(obj)
    } )
 
@@ -38,16 +39,23 @@ const SearchBar = ({openSearchBox,setOpenSearchBox}) => {
         setKeyword("")
         setOpenSearchBox(false)
     }
-
-
+    const handleMouseEnter= (e) => {
+        e.target.className= "text-white my-1 bg-black p-2 rounded-sm"
+    }
+    const handleMouseLeave= (e) => {
+        e.target.className= "text-black  my-1 bg-gray-200 p-2 rounded-sm"
+    }
+    const bgColor = filterNews(searchList).length ? 'bg-gray-50' : null 
     return  ReactDOM.createPortal(      
         <div className="bg-gray-500	absolute inset-0 bg-opacity-40 "  >
-            <div className=" w-4/12 fixed top-1/3 left-1/3 mx-auto" >
+            <div className=" w-4/12 fixed top-1/4 left-1/3 mx-auto" >
                 <button><Close className="cursor-pointer" onClick={handleClose}/></button>
                 <Input placeholder="Search for an article." prefix={<Search size={16} />} value={keyword} onChange={handleChange}/>  
-                <div className="">
+                <div className={`my-3  ${bgColor} p-3 rounded flex flex-col max-h-96 overflow-clip overflow-hidden overflow-y-auto`}>
                     {
-                        filterNews(searchList).map(({title}) => <a href="#" className="">{title}</a>)
+                        filterNews(searchList).map(({title,url,category, author, content,date, readMoreUrl, details}) => <Link to={{ pathname:`/article/${url.slice(33)}`,state: {img_src:"https://picsum.photos/id/164/520/260",category, title,url,category, author, content,date, readMoreUrl,fullnews:details }  }} className="text-black my-1 bg-gray-200 p-2 rounded-sm" onMouseLeave={handleMouseLeave} onMouseEnter={handleMouseEnter}>
+                            {title}
+                        </Link>)
                     }
                 </div>            
             </div>
--- src/components/SearchResult.jsx
+++ src/components/SearchResult.jsx
@@ -1,9 +1,10 @@
+
 import React from 'react'
 import {Link} from 'react-router-dom'
 
 const SearchResult = () => {
     return (
-        <Link to={{ pathname:`/article/${data.url.slice(33)}`,state: {img_src:"https://picsum.photos/id/164/520/260",category, ...data,fullnews}  }} className="mt-3 text-read_more">Read More</Link>
+        <Link to={{ pathname:`/article/${url.slice(33)}`,state: {img_src:"https://picsum.photos/id/164/520/260",category, title,url,category, author, content,date, readMoreUrl,fullnews:details }  }} className="text-black my-1 bg-gray-200 p-2 rounded-sm">{title}</Link>
     )
 }
 
