fbad7ff43e7a53c7a6f6faedd74cbafc1ff47a9d
ashiajoseph <ashiajoseph@gmail.com>
2021-10-30 10:47:20 +0530

    Debouncing done

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,15 +1,18 @@
-import React, {setState, useEffect, useState, useContext} from 'react'
+import React, {useState, useEffect, useContext, useCallback} from 'react'
 import ReactDOM from "react-dom"
 import { Input } from "@bigbinary/neetoui/v2";
 import { Search,Close,Right } from "@bigbinary/neeto-icons";
 import { categoryContext } from './categoryContext';
 import { newsContext } from './Landing';
 import {Link} from 'react-router-dom'
-
+import { debounce } from 'lodash';
 const SearchBar = ({openSearchBox,setOpenSearchBox}) => {
     const [keyword, setKeyword] = useState("")
     const {categoryCheckbox}= useContext(categoryContext)
     const newsDetails = useContext(newsContext)
+    const deb = useCallback( debounce((searchword) => {setKeyword(searchword) 
+    
+    },500),[])
     if(!openSearchBox) return <div></div>
    
     let searchList =[]
@@ -22,25 +25,25 @@ const SearchBar = ({openSearchBox,setOpenSearchBox}) => {
       searchList= searchList.concat(obj)
    } )
 
-   const filterNews= (searchList) => {
-       
+    const filterNews= (searchList) => {    
         let filteredList= []
         if(keyword)
             filteredList= searchList.filter(({title}) => title.toLowerCase().includes(keyword))
         return filteredList
    }
+
+   
      
-   const handleChange= (e) => {
-       const searchword = e.target.value.toLowerCase()
-        setKeyword(searchword)
-        
-    }
+   const handleChange= (searchword) => {
+        deb(searchword.toLowerCase())
+     }
+
     const handleClose= () => {
         setKeyword("")
         setOpenSearchBox(false)
     }
     const handleMouseEnter= (e) => {
-        e.target.className= "text-white my-1 bg-black p-2 rounded-sm"
+        e.target.className= "text-white my-1 bg-black p-2 rounded-sm bg-opacity-90"
     }
     const handleMouseLeave= (e) => {
         e.target.className= "text-black  my-1 bg-gray-200 p-2 rounded-sm"
@@ -49,8 +52,8 @@ const SearchBar = ({openSearchBox,setOpenSearchBox}) => {
     return  ReactDOM.createPortal(      
         <div className="bg-gray-500	absolute inset-0 bg-opacity-40 "  >
             <div className=" w-4/12 fixed top-1/4 left-1/3 mx-auto" >
-                <button><Close className="cursor-pointer" onClick={handleClose}/></button>
-                <Input placeholder="Search for an article." prefix={<Search size={16} />} value={keyword} onChange={handleChange}/>  
+                <button><Close className="cursor-pointer absolute left-full top-5 	" size={28} onClick={handleClose}/></button>
+                <Input placeholder="Search for an article." prefix={<Search size={16} />}  onChange={(e) => handleChange(e.target.value)}/>  
                 <div className={`my-3  ${bgColor} p-3 rounded flex flex-col max-h-96 overflow-clip overflow-hidden overflow-y-auto`}>
                     {
                         filterNews(searchList).map(({title,url,category, author, content,date, readMoreUrl, details}) => <Link to={{ pathname:`/article/${url.slice(33)}`,state: {img_src:"https://picsum.photos/id/164/520/260",category, title,url,category, author, content,date, readMoreUrl,fullnews:details }  }} className="text-black my-1 bg-gray-200 p-2 rounded-sm" onMouseLeave={handleMouseLeave} onMouseEnter={handleMouseEnter}>
